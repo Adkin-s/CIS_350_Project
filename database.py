7bf688e620cdf3a7c05a2addb740a4f54abfb7f9
Elijah Morgan <morgaeli@mail.gvsu.edu>
2023-10-22 00:53:43 -0400

    Basic Email Authentication Classes and Methods

---
+++ database.py
@@ -0,0 +1,11 @@
+import pyrebase
+
+firebaseConfig = {
+    'apiKey': "AIzaSyCXRXtK07m6wmrX4ZPhzYXevo_Dv2MCENI",
+    'authDomain': "snrk-a6050.firebaseapp.com",
+    'databaseURL': "https://snrk-a6050-default-rtdb.firebaseio.com",
+    'projectId': "snrk-a6050",
+    'storageBucket': "snrk-a6050.appspot.com",
+    'messagingSenderId': "227455950615",
+    'appId': "1:227455950615:web:ec7caa4f3a41168c936152"
+}
