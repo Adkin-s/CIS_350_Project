9ebd6623e264c91fa3c2f1e97742333ce3058531
Elijah Morgan <morgaeli@mail.gvsu.edu>
2023-11-14 03:00:46 -0500

    Fixed some syntax errors with Persona().prompt(), added copy library to fix issues with messages failing to interact with API correctly.

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 import openai
+import copy
 
 #TODO Add error handling for no response from API.
 
@@ -12,29 +13,38 @@ class Persona():
         self.personality_statement = personality_statement
         
         # Checks if first message is the persona statement of the AI.
-        if self.message_history[0] != {"role": "system", "content": self.personality_statement}: 
+        if self.message_history[0] != {"role": "system", "content": self.personality_statement}:
             self.message_history.insert(0, {"role": "system", "content": self.personality_statement})
+            self.message_history.pop(1)
 
     def updateDB(self, email, message_history) -> None:
         pass
         #TODO make this function update the database with the message history.
     
     def prompt(self, prompt: str) -> str:
-        formattedPrompt = {"role": "user", "content": prompt}
-        self.message_history.append(formattedPrompt)
+        self.message_history.append({"role": "user", "content": prompt})
 
-        # Limit message history to 10 messages.
+        # Limit message history to 10 messages (not including personality statement).
         if len(self.message_history) > 11:
             self.message_history.pop(1)
 
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=self.message_history,
-            maximum_tokens=50
-            )
+                                                model="gpt-3.5-turbo",
+                                                messages=copy.deepcopy(self.message_history),
+                                                max_tokens=100
+                                                )
         
         self.message_history.append(response)
         #TODO updateDB() with message history.
 
         return response.choices[0]["message"]["content"]
-    
+
+'''
+#Test persona and prompt, feel free to play around. Output will is a bit cluttered, but the metadata may be useful.
+
+Snarky = Persona("Snarky", "You are a mean, snarky, and passive-agressive assistant.", [{}])
+print("Personality Statement: " + Snarky.personality_statement, "\nMessage history after: ", Snarky.message_history)
+print(Snarky.prompt("Why is school so hard?"))
+print("Message history after: ", Snarky.message_history)
+print("Chatbot response: ", Snarky.message_history[-1]["choices"][0]["message"]["content"])
+'''
